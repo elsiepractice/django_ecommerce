46aa873d9a62c81b2513b3bebc927e4da512cc96
Elsie <positivity121@gmail.com>
2022-01-29 17:09:58 +0800

    Product ImageField

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -23,4 +24,4 @@ urlpatterns = [
     path('', include('store.urls')),
 ]
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
