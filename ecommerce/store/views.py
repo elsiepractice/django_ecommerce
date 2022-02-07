7f97c78bd1910b24122fe079fcc85880a7734adb
Elsie <positivity121@gmail.com>
2022-02-07 21:32:07 +0800

    Process Order

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,7 +2,7 @@ from tkinter import E
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-
+import datetime
 from .models import *
 
 #Create yout views here.
@@ -44,6 +44,7 @@ def checkout(request):
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
 		cartItems = order.get_cart_items
+		print('cartItems',order.get_cart_items)
 	else:
 		items = []
 		order = {'get_cart_items':0,'get_cart_total':0,'shipping':False}
@@ -77,3 +78,33 @@ def updateItem(request):
 		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total():
+			order.complete = True
+		else:
+			print(total,'and ',order.get_cart_total())
+		order.save()
+
+		#print('order.shipping',order.shipping())
+		if order.shipping() == True:
+			ShippingAddress.objects.create(
+				customer=customer,
+				order=order,
+				address=data['shipping']['address'],
+				city=data['shipping']['city'],
+				state=data['shipping']['state'],
+				zipcode=data['shipping']['zipcode'],
+			)
+	else:
+		print('User is not logged in..')
+	return JsonResponse('Payment complete!',safe=False)
